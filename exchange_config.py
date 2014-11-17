be95ae3f4a6d78a2ab96c8301a74353e3e148b89
ira <ira@coinapult.com>
2014-11-17 11:22:46 -0500

    remove unused config variables

--- exchange_config.py
+++ exchange_config.py
@@ -1,34 +1,23 @@
 exchange_config = {
     'btcchina': {
         'live': True,
-        'trade_fee': 0.00,
-        'dominant_pair': "btccny",
         'api_keys': {'key': '',  'secret': ''}
     },
     'bitstamp': {
         'live': True,
-        'trade_fee': 0.002,
-        'dominant_pair': "btcusd",
         'api_keys': {'key': '',  'secret': ''}
     },
     'bitfinex': {
         'live': True,
-        'withdrawal_fee_range': [0, 0],
-        'dominant_pair': "btcusd",
         'api_keys': {'key': '', 'secret': ''}
     },
     'btce': {
         'live': True,
-        'trade_fee': 0.002,
-        'dominant_pair': "btcusd",
-        'api_keys': {'key': '',
-                     'secret': ''}
+        'api_keys': {'key': '', 'secret': ''}
 
     },
     'kraken': {
         'live': True,
-        'trade_fee': 0.005,
-        'dominant_pair': "btceur",
         'api_keys': {'key': '', 'secret': ''}
     }
 }
