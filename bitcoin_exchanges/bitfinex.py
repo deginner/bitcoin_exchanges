a249c35dbb5b709513a24906a322290bfb7bb07f
d3brouille <debrouille@manlymail.net>
2016-04-30 10:33:00 -0500

    multicurrency support, ref: https://github.com/deginner/bitcoin_exchanges/issues/12

--- bitcoin_exchanges/bitfinex.py
+++ bitcoin_exchanges/bitfinex.py
@@ -17,7 +17,6 @@ REQ_TIMEOUT = 10  # seconds
 
 class Bitfinex(ExchangeABC):
     name = 'bitfinex'
-    fiatcurrency = 'USD'
 
     def __init__(self, key, secret):
         super(Bitfinex, self).__init__()
@@ -49,12 +48,53 @@ class Bitfinex(ExchangeABC):
                 raise ExchangeError('bitfinex', '%s %s while sending to bitfinex %r' % (type(e), str(e), params))
         return response
 
-    def cancel_order(self, order_id):
+    @classmethod
+    def format_pair(cls, pair):
+        """
+        formatted : unformatted
+        'BTC_USD': 'btcusd'
+        'LTC_USD': 'ltcusd'
+        'LTC_BTC': 'ltcbtc'
+        'ETH_USD': 'ethusd'
+        'ETH_BTC': 'ethbtc'
+        """
+        if pair[0] == 'l':
+            base = 'LTC'
+            if pair[2:] == 'btc':
+                quote = 'BTC'
+            else:
+                quote = 'USD'
+        elif pair[0] == 'e':
+            base = 'ETH'
+            if pair[2:] == 'btc':
+                quote = 'BTC'
+            else:
+                quote = 'USD'
+        elif pair[0] == 'b':
+            base = 'BTC'
+            quote = 'USD'
+        return base + '_' + quote
+
+    @classmethod
+    def unformat_pair(cls, pair):
+        exch_pair = pair[:3].lower() + pair[4:].lower()
+        return exch_pair
+
+    def base_currency(self, pair):
+        bcurr = pair[:3]
+        return bcurr
+
+    def quote_currency(self, pair):
+        qcurr = pair[4:]
+        return qcurr
+
+    def cancel_order(self, order_id, pair=None):
         params = {'order_id': int(order_id)}
         try:
             resp = self.bitfinex_request('/v1/order/cancel', params).json()
         except ValueError as e:
             raise ExchangeError('bitfinex', '%s %s while sending to bitfinex %r' % (type(e), str(e), params))
+        print resp
         if resp and 'id' in resp and resp['id'] == params['order_id']:
             return True
         elif 'message' in resp and resp['message'] == 'Order could not be cancelled.':
@@ -62,14 +102,16 @@ class Bitfinex(ExchangeABC):
         else:
             return False
 
-    def cancel_orders(self, **kwargs):
+    def cancel_orders(self, pair=None, **kwargs):
         resp = self.bitfinex_request('/v1/order/cancel/all')
-        if "All orders cancelled" in resp.text:
+        # example respose:
+        # {"result":"4 orders successfully cancelled"}
+        if "orders successfully cancelled" in resp.text:
             return True
         else:
             return False
 
-    def create_order(self, amount, price, otype, typ='exchange limit', bfxexch='all'):
+    def create_order(self, amount, price, otype, pair, typ='exchange limit', bfxexch='all'):
         if BLOCK_ORDERS:
             return "order blocked"
         if otype == 'bid':
@@ -78,10 +120,10 @@ class Bitfinex(ExchangeABC):
             otype = 'sell'
         else:
             raise Exception('unknown side %r' % otype)
-
+        exch_pair = exchange.unformat_pair(pair)
         params = {
             'side': otype,
-            'symbol': 'btcusd',
+            'symbol': exch_pair,
             'amount': "{:0.3f}".format(amount),
             'price': "{:0.3f}".format(price),
             'exchange': bfxexch,
@@ -108,10 +150,10 @@ class Bitfinex(ExchangeABC):
         try:
             data = self.bitfinex_request('/v1/balances').json()
         except ValueError as e:
-            raise ExchangeError('bitfinex', '%s %s while sending to bitfinex get_open_orders' % (type(e), str(e)))
+            raise ExchangeError('bitfinex', '%s %s while sending to bitfinex get_balance' % (type(e), str(e)))
         if 'message' in data:
             raise ExchangeError(exchange='bitfinex', message=data['message'])
-        relevant = filter(lambda x: x['currency'] in ('usd', 'btc'), data)
+        relevant = filter(lambda x: x['currency'] in ('usd', 'btc', 'ltc'), data)
 
         if btype == 'total':
             total = MultiMoney(*map(lambda x: Money(x['amount'], x['currency'].upper()), relevant))
@@ -124,39 +166,46 @@ class Bitfinex(ExchangeABC):
             available = MultiMoney(*map(lambda x: Money(x['available'], x['currency'].upper()), relevant))
             return total, available
 
-    def get_open_orders(self):
+    def get_open_orders(self, pair):
+        exch_pair = exchange.unformat_pair(pair)
         try:
             rawos = self.bitfinex_request('/v1/orders').json()
         except ValueError as e:
             raise ExchangeError('bitfinex', '%s %s while sending to bitfinex get_open_orders' % (type(e), str(e)))
         orders = []
         for o in rawos:
-            side = 'ask' if o['side'] == 'sell' else 'bid'
-            orders.append(MyOrder(Money(o['price'], self.fiatcurrency), Money(o['remaining_amount']), side,
+            if o['symbol'] == exch_pair:
+                side = 'ask' if o['side'] == 'sell' else 'bid'
+                orders.append(MyOrder(Money(o['price'], exchange.quote_currency(pair)), Money(o['remaining_amount'], exchange.base_currency(pair)), side,
                                   self.name, str(o['id'])))
+            else:
+                pass
         return orders
 
     @classmethod
-    def get_order_book(cls, pair='btcusd', **kwargs):
+    def get_order_book(cls, pair=None, **kwargs):
+        exch_pair = exchange.unformat_pair(pair)
         try:
-            return requests.get('%s/v1/book/%s' % (BASE_URL, pair),
+            return requests.get('%s/v1/book/%s' % (BASE_URL, exch_pair),
                                 timeout=REQ_TIMEOUT).json()
         except ValueError as e:
             raise ExchangeError('bitfinex', '%s %s while sending to bitfinex get_order_book' % (type(e), str(e)))
 
     @classmethod
-    def get_ticker(cls, pair='btcusd'):
+    def get_ticker(cls, pair=None):
+        exch_pair = exchange.unformat_pair(pair)
         try:
-            rawtick = requests.get(BASE_URL + '/v1/pubticker/%s' % pair, timeout=REQ_TIMEOUT).json()
+            rawtick = requests.get(BASE_URL + '/v1/pubticker/%s' % exch_pair, timeout=REQ_TIMEOUT).json()
         except (ConnectionError, Timeout, ValueError) as e:
             raise ExchangeError('bitfinex', '%s %s while sending get_ticker to bitfinex' % (type(e), str(e)))
 
         return create_ticker(bid=rawtick['bid'], ask=rawtick['ask'], high=rawtick['high'], low=rawtick['low'],
                              volume=rawtick['volume'], last=rawtick['last_price'], timestamp=rawtick['timestamp'],
-                             currency='USD')
+                             currency=exchange.quote_currency(pair), vcurrency=exchange.base_currency(pair))
 
-    def get_transactions(self, limit=None):
-        params = {'symbol': 'BTCUSD'}
+    def get_transactions(self, pair, limit=None):
+        exch_pair = exchange.unformat_pair(pair)
+        params = {'symbol': exch_pair}
         try:
             return self.bitfinex_request('/v1/mytrades', params).json()
         except ValueError as e:
@@ -177,6 +226,25 @@ class Bitfinex(ExchangeABC):
                 type(e), str(e), str(order_id)))
 
     def get_deposit_address(self):
+        """
+        # TODO implement for multicurrency
+
+        request parmas
+        Key         Type      Description
+        method	    [string]  Method of deposit (accepted:
+                                  'bitcoin', 'litecoin', 'ethereum'.)
+        wallet_name [string]  Your wallet needs to already exist.
+                              Wallet to deposit in (accepted:
+                                  'trading', 'exchange', 'deposit')
+        renew	    [integer] (optional) Default is 0.
+                              If set to 1, will return a new unused deposit address
+
+        response
+        result	    [string]   'success' or 'error
+        method	    [string]
+        currency    [string]
+        address	    [string]	The deposit address (or error message if result = 'error')
+        """
         try:
             result = self.bitfinex_request('/v1/deposit/new', {'currency': 'BTC', 'method': 'bitcoin',
                                                              'wallet_name': 'exchange'}).json()
@@ -193,7 +261,6 @@ class Bitfinex(ExchangeABC):
         except ValueError as e:
             raise ExchangeError('bitfinex', '%s %s while sending to bitfinex get_open_orders' % (type(e), str(e)))
         return data
-
 
 eclass = Bitfinex
 exchange = Bitfinex(exchange_config['bitfinex']['api_creds']['key'],
